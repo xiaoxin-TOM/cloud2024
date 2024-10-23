347716a527cbf4ac64aa2d315f050851175529f5
tom <xiaoxin1995ok@qq.com>
2024-10-23 09:13:17 +0800

    支付模块修改

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.atguigu.cloud") //import tk.mybatis.spring.annotation.MapperScan;
+@MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
 public class Main8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
