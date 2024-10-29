30f1538c02b2f6592facfed219c8b878228440e9
tom <xiaoxin1995ok@qq.com>
2024-10-29 10:16:33 +0800

    统一controller返回数据

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud.controller;
 
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.PayDto;
+import com.atguigu.cloud.resp.ResultData;
 import com.atguigu.cloud.service.PayService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -17,33 +18,51 @@ public class PayController {
     PayService payService;
 
     @PostMapping("/pay/add")
-    public String add(@RequestBody Pay pay){
+    public ResultData<Integer> add(@RequestBody Pay pay){
         int i = payService.add(pay);
-        System.out.println(pay.toString());
-        return "插入返回成功值:"+i;
+        new ResultData<>();
+        ResultData<Integer> resultData;
+        if( i > 0){
+            resultData = ResultData.success(i);
+        }else{
+            resultData = ResultData.fail("500","数据插入失败");
+        }
+        return resultData;
     }
 
     @DeleteMapping("/pay/delete/{id}")
-    public String delete(@PathVariable("id") Long id){
+    public ResultData<Integer> delete(@PathVariable("id") Long id){
+        ResultData<Integer> resultData;
         int i = payService.delete(id);
-        return "删除返回成功值:"+i;
+        if(i > 0){
+            resultData = ResultData.success(i);
+        }else{
+            resultData = ResultData.fail("500","数据删除失败");
+        }
+        return resultData;
     }
 
     @PutMapping("/pay/update")
-    public String update(@RequestBody PayDto payDto){
+    public ResultData<Integer> update(@RequestBody PayDto payDto){
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDto,pay);
+        ResultData<Integer> resultData;
         int i = payService.update(pay);
-        return "更新返回成功值:"+i;
+        if(i > 0){
+            resultData = ResultData.success(i);
+        }else{
+            resultData = ResultData.fail("500","数据更新失败");
+        }
+        return resultData;
     }
 
     @GetMapping("/pay/get/{id}")
-    public Pay queryById(@PathVariable Long id){
-        return payService.getPayById(id);
+    public ResultData<Pay> queryById(@PathVariable Long id){
+        return ResultData.success(payService.getPayById(id));
     }
 
     @GetMapping("/pay/getAll")
-    public List<Pay> queryAll(){
-        return payService.getPayList();
+    public ResultData<List<Pay>> queryAll(){
+        return ResultData.success(payService.getPayList());
     }
 }
