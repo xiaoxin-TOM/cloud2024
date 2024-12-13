106d53f51632173b169196850ee0ed57819d03b2
xiaoxinTOM <xiaoxin1995ok@qq.com>
2024-12-13 16:44:59 +0800

    新增的接口

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -3,9 +3,7 @@ package com.atguigu.cloud.controller;
 import com.atguigu.cloud.entities.PayDto;
 import com.atguigu.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -20,6 +18,12 @@ public class OrderController {
     @PostMapping("/consumer/pay/add")
     public ResultData addOrd(@RequestBody PayDto payDto){
         return restTemplate.postForObject(url+"/pay/add",payDto,ResultData.class);
+    }
+
+    @GetMapping("/consumer/get/{id}")
+    public ResultData getOrd(@PathVariable String id){
+//        return restTemplate.postForObject(url+"/pay/get/",id,ResultData.class);
+        return restTemplate.getForObject(url+"/pay/get/"+id,ResultData.class);
     }
 
 
